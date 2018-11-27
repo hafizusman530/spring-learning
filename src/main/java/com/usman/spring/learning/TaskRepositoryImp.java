c77c38435fafd7e7d25c7240667977e0b281863b
Muhammad Usman <muhammad.usman@tacton.com>
2018-11-27 14:49:35 +0500

    Add REST crud methods

--- src/main/java/com/usman/spring/learning/TaskRepositoryImp.java
+++ src/main/java/com/usman/spring/learning/TaskRepositoryImp.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class TaskRepositoryImp implements TaskRepository {
@@ -25,16 +26,27 @@ public class TaskRepositoryImp implements TaskRepository {
 
     @Override
     public Task getTask(String id) {
-        return null;
+        return taskList.stream()
+                .filter(task -> task.getId().equals(id))
+                .findAny()
+                .orElse(null);
     }
 
     @Override
     public void removeTask(String id) {
-
+        taskList.addAll(
+                taskList.stream()
+                        .filter(task -> task.getId().equals(id))
+                        .collect(Collectors.toList())
+        );
     }
 
     @Override
-    public void updateTask(Task task) {
-
+    public void updateTask(Task toUpdate) {
+        taskList.addAll(
+                taskList.stream()
+                        .filter(task -> task.getId().equals(toUpdate.getId()))
+                        .collect(Collectors.toList())
+        );
     }
 }
--- src/main/java/com/usman/spring/learning/TodoController.java
+++ src/main/java/com/usman/spring/learning/TodoController.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @ResponseBody
+@RequestMapping(path = "/todo/tasks")
 public class TodoController {
     private final TaskRepository taskRepository;
 
@@ -22,23 +22,35 @@ public class TodoController {
         this.taskRepository = taskRepository;
     }
 
-    //  @RequestBody
-    //  @HttpEntity<Type>
-    @RequestMapping(path = "/todo/tasks", method = RequestMethod.POST, consumes = "application/json")
+    @RequestMapping(path = "/", method = RequestMethod.POST, consumes = "application/json")
     public ResponseEntity<String> create(@RequestBody Task task) {
         taskRepository.addTask(task);
         return new ResponseEntity<>("Task Created with id " + task.getId(), HttpStatus.OK);
     }
 
-    @RequestMapping(path = "/todo/tasks", method = RequestMethod.GET, consumes = "application/json")
-    public ResponseEntity<List<Task>> create() {
+    @RequestMapping(path = "/", method = RequestMethod.PUT, consumes = "application/json")
+    public ResponseEntity<String> update(@RequestBody Task task) {
+        taskRepository.updateTask(task);
+        return new ResponseEntity<>("Task updated with id " + task.getId(), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.DELETE, consumes = "application/json")
+    public ResponseEntity<String> delete(@PathVariable String taskId) {
+        taskRepository.removeTask(taskId);
+        return new ResponseEntity<>("Task Removed with id " + taskId, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET, consumes = "application/json")
+    public ResponseEntity<List<Task>> get() {
         List<Task> taskList = taskRepository.getTasks();
         return new ResponseEntity<>(taskList, HttpStatus.OK);
     }
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    ResponseEntity<String> helloWorld() {
-        return new ResponseEntity<String>("Hello World", HttpStatus.OK);
+    @RequestMapping(path = "/{taskId}", method = RequestMethod.GET, consumes = "application/json")
+    public ResponseEntity<Task> getSpecificTask(@PathVariable String taskId) {
+        Task task = taskRepository.getTask(taskId);
+        return new ResponseEntity<>(task, HttpStatus.OK);
     }
+
 
 }
